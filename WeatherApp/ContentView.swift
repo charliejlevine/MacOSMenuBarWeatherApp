9f166eaf59abb53a22b84b2b3e910279387ab1c4
charlielevine <charliechamp616@gmail.com>
2021-03-02 00:43:43 -0500

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Charlie Levine on 3/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
